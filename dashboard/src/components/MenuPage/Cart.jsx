8c8f4a9536d2dcdbcc15eb21e60a8484779c483a
oparap8 <oparap8@gmail.com>
2025-10-10 13:52:18 +0200

    feat: menu categories  backend zustand implementation

--- dashboard/src/components/MenuPage/Cart.jsx
+++ dashboard/src/components/MenuPage/Cart.jsx
@@ -46,7 +46,7 @@ const Cart = () => {
 
   return (
     <>
-      <Card className="h-[750px] flex flex-col">
+      <Card className="h-[820px] flex flex-col">
         <CardHeader>
           <CardTitle className="flex justify-between items-center">
             <Clock />
