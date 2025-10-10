2eded8e0ce54ade94e6966bf9e146025db1ad537
oparap8 <oparap8@gmail.com>
2025-10-10 14:42:51 +0200

    feat: menu page  backend zustand implementation

--- dashboard/src/components/MenuPage/MenuItemCard.jsx
+++ dashboard/src/components/MenuPage/MenuItemCard.jsx
@@ -3,6 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { ShoppingCart, Plus, Minus } from "lucide-react";
 import { MenuCartContext } from "@/routes/pages/MenuPage";
 import NumPad from "./UpdateCartDialog";
+import { formatCurrency } from "@/lib/utils";
 
 const MenuItemCard = ({ item }) => {
 
@@ -18,8 +19,8 @@ const MenuItemCard = ({ item }) => {
         className="cursor-pointer rounded-lg border shadow-sm transition transform hover:shadow-md hover:scale-[1.02] active:scale-[0.98] active:bg-gray-50"
       >
         <CardHeader className="flex items-center justify-between">
-          <CardTitle className="text-lg">{item.name}</CardTitle>
-          <i>${item.price}</i>
+          <CardTitle>{item.item_name}</CardTitle>
+          <i>{formatCurrency(item.standard_rate)}</i>
         </CardHeader>
       </Card>
     </>
